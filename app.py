e0279476e31284e58da2627c63ae0d48a2f7594f
Kong-Chen <51734766+Kong-Chen@users.noreply.github.com>
2023-06-06 18:08:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,11 @@ app = Flask(__name__)
 
 # 設置你的 LINE Bot 的 Channel Access Token 和 Channel Secret
 line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-
-# line_bot_api = LineBotApi(os.environ['czH9a2VRdE4WY0n031h2sMjVEs979haELbdnS1QTvd8WooM7wMtulpBp1sTMqDua60W0Gq6M0oo+gH6hkcfAuyTiUFjq1Xrlhi+Pkts+9p9AsbdGNqAO2oGhK3AlGhZuf9NKV+QVtUTENFTWJG0wNAdB04t89/1O/w1cDnyilFU='])
-# handler = WebhookHandler(os.environ['cb67a671669fd689df93422edd6fea22'])
+handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-
     # 取得 request headers 中的 X-Line-Signature 屬性
     signature = request.headers['X-Line-Signature']
     
@@ -31,6 +27,7 @@ def callback():
     
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 收到使用者的訊息
@@ -42,5 +39,7 @@ def handle_message(event):
         TextSendMessage(text=user_message)
     )
 
+
 if __name__ == "__main__":
-    app.run()
+    # 在本地運行時才啟動伺服器
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
