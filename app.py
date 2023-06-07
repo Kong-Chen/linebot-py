78cf6fc7c1342d55a4094f93f79b648c428e0701
Kong-Chen <51734766+Kong-Chen@users.noreply.github.com>
2023-06-07 17:46:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,6 @@ def handle_message(event):
     connection.commit()
     # rows = cursor.fetchall()
 
-
 if __name__ == "__main__":
     # 在本地運行時才啟動伺服器
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
